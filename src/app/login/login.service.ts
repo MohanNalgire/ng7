88039b9bf470ceab7952e208120679b416f5952f
Mohan Nalgire <mohan_nalgire@persistent.co.in>
2019-05-10 15:38:03 +0530

    updated for testing.

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -19,7 +19,7 @@ export class LoginService {
 
 
   allUsers: Array<User>;
-  private apiURL = 'http://localhost:3000/user'
+  private apiURL = 'http://localhost:3000/users'
 
   objBody = {
     "id": 0,
