e0ee93a746e40e57b8350930eec7976ecbffcb23
Mohan Nalgire <mnalgire@gmail.com>
2020-05-06 18:08:42 +0530

    Updated proxy and constant

--- frontend/src/app/core/urls.constant.ts
+++ frontend/src/app/core/urls.constant.ts
@@ -1,9 +1,9 @@
 export const CUSTOM_URLS = {
-  urlconfigEnv:'http://localhost:4100/environments',
-  urlQuestions:'http://localhost:4100/questions',
-  urlTechnologies: 'http://localhost:4100/technologies',
-  urlUsers: 'http://localhost:4100/users',
-  urlDiffrence: 'http://localhost:4100/diffrence',
-  urlHeaderMenu: 'http://localhost:4100/headerMenu',
-  urlPageMetaTags:'http://localhost:4100/pageMetaTags'
+  urlconfigEnv:'http://localhost:4100/api/v1/environments',
+  urlQuestions:'http://localhost:4100/api/v1/questions',
+  urlTechnologies: 'http://localhost:4100/api/v1/technologies',
+  urlUsers: 'http://localhost:4100/api/v1/users',
+  urlDiffrence: 'http://localhost:4100/api/v1/diffrence',
+  urlHeaderMenu: 'http://localhost:4100/api/v1/headerMenu',
+  urlPageMetaTags:'http://localhost:4100/api/v1/pageMetaTags'
 };
